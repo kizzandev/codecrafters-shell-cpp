1dbe17f7b60a6f10d505593b45d70afec93264f3
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-03 19:47:18 -0300

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  std::cerr << std::unitbuf;  
 
-  std::cout << "$ ";
-  
-  std::string input;
-  std::getline(std::cin, input);
+  while (1) {
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
 
-  std::cerr << input << ": command not found\n";
+    std::cerr << input << ": command not found" << std::unitbuf;
+  }
 }
