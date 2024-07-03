bd059ef871d161460370c7d6df42c6b85e8e717d
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-03 19:33:38 -0300

    pass 1st stage

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,8 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  //
-  // std::string input;
-  // std::getline(std::cin, input);
+  std::cout << "$ ";
+  
+  std::string input;
+  std::getline(std::cin, input);
 }
