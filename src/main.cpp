974979178057bd0a2c300de6dac5b84fbe672a74
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-03 20:09:19 -0300

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,7 @@ int main() {
 
   std::cout << "$ ";
   std::string input;
-  while (input.find("exit") != 0) {
-    std::getline(std::cin, input);
-
+  while (std::getline(std::cin, input) && input.find("exit") != 0) {
     if (input.find("echo ") == 0) std::cout << input.substr(5) << '\n' << std::unitbuf;
     else std::cerr << input << ": command not found\n" << std::unitbuf;
     std::cout << "$ ";
